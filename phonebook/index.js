51981099f493a63ca4a92803d90f1d9fe239e29a
salaamander <saghevli@gmail.com>
2020-09-13 02:08:28 -0400

    3.4 solution

--- phonebook/index.js
+++ phonebook/index.js
@@ -47,6 +47,12 @@ app.get('/api/persons/:id', (request, response) => {
   }
 })
 
+app.delete('/api/persons/:id', (request, response) => {
+  persons = persons.filter(person => person.id !== Number(request.params.id))
+
+  response.status(204).end()
+})
+
 const PORT = 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
