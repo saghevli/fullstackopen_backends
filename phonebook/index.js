409eeac47b1c69e3f260b24ea75597ee60134049
salaamander <saghevli@gmail.com>
2020-09-13 01:56:19 -0400

    3.2 solution

--- phonebook/index.js
+++ phonebook/index.js
@@ -33,6 +33,10 @@ app.get('/api/persons', (req, res) => {
   res.json(persons)
 })
 
+app.get('/info', (req, res) => {
+  res.send(`Phonebook has info for ${persons.length} people </br> ${new Date()}`)
+})
+
 const PORT = 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
