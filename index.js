19d514843891a6cba3b9df3110ca981c3ea7ae7d
salaamander <saghevli@gmail.com>
2020-09-21 00:17:12 -0400

    3.10 solution and 3.9 in one

--- index.js
+++ index.js
@@ -2,7 +2,10 @@ const express = require('express')
 const app = express()
 const morgan = require('morgan')
 
+// express for routing to static FE and to bundle JS responses
 app.use(express.json())
+app.use(express.static('build'))
+// morgan to log requests
 app.use( morgan(function (tokens, req, res) {
   let output = [
     tokens.method(req, res),
@@ -16,7 +19,6 @@ app.use( morgan(function (tokens, req, res) {
   }
   return output.join(' ')
 }))
-app.use(express.static('build'))
 
 let persons =
   [
